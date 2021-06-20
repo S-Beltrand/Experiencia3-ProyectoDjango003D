63d9a85d0d83cd68b8f1c566f11fa185c0b7e477
S-Beltrand <80703838+S-Beltrand@users.noreply.github.com>
2021-06-19 20:43:42 -0400

    creación de usuario creada

---
+++ experienciaDjango3 003D/VisualGeek/core/forms.py
@@ -0,0 +1,69 @@
+from django import forms
+from django.forms import ModelForm
+from django.forms import widgets
+from django.forms.models import ModelChoiceField
+from django.forms.widgets import Widget
+from . models import Categoria, Emisor
+
+
+class EmisorForm(forms.ModelForm):
+
+    class Meta: 
+        model= Emisor
+        fields = ['nombre', 'apellido', 'edad', 'email', 'resumen','categoria']
+        labels ={
+            'nombre': 'Nombbre', 
+            'apellido': 'Apellido', 
+            'edad': 'Edad', 
+            'email': 'Email',
+            'categoria': 'Categoria',
+            'resumen': 'Resumen',
+        }
+        widgets={
+            'email': forms.TextInput(
+                attrs={
+                    'class': 'barra', 
+                    'placeholder': 'Ingrese email', 
+                    'id': 'email'
+                }
+            ), 
+            'edad': forms.TextInput(
+                attrs={
+                    'class': 'barra', 
+                    'placeholder': 'Ingrese edad', 
+                    'id': 'edad'
+                }
+            ), 
+            'nombre': forms.TextInput(
+                attrs={
+                    'class': 'barra', 
+                    'placeholder': 'Ingrese nombre', 
+                    'id': 'nombre'
+                }
+            ), 
+            'apellido': forms.TextInput(
+                attrs={
+                    'class': 'barra', 
+                    'placeholder': 'Ingrese apellido', 
+                    'id': 'apellido'
+                }
+            ), 
+            'categoria': forms.Select(
+                attrs={
+                    'class': 'barra',
+                    'id': 'categoria',
+                }
+            ),
+            'resumen': forms.TextInput(
+                attrs={
+                    'class': 'barra', 
+                    'placeholder': 'Ingrese resumen', 
+                    'id': 'resumen'
+                }
+            ), 
+
+        }
+
+ 
+    
+     
